1218bcf1c73eb352eadadcdb6f1496d2bfd6443e
NursIsma <144454802+NursIsma@users.noreply.github.com>
2023-10-10 15:22:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+#Ряд1
+#A = int(input('chislo 1'))
+#B = int(input('chislo 2'))
+#for i in range(A, B + 1):
+    #print(i)
+#Сумма N числа
+#N = int(input('2 chislo'))
+#sum = 1
+#for i in range(N):
+  #  num = int(input('3 chislo'))
+ #   sum += num
+#print(sum)
+
+
+#Ряд 2
+#A = int(input('2 chislo'))
+#B = int(input('3 chislo'))
+#if A < B:
+ # for i in range(A, B+1):
+ #   print(i)
+#else:
+ # for i in range(A, B-1, -1):
+#    print(i)
+
+  # Лесенка
+#n = int(input('3 chislo'))
+#for i in range(1, n + 1):
+ #   stair = ""
+  #  for j in range(1, i + 1):
+   #     stair += str(j)
+    #print(stair)
+
+
+ #  Степень двойки
+#n = int(input())
+#st = 0
+#k = 1
+#while k <= n//2:
+  #st += 1
+ # k *= 2
+#print(st, k)
+
+
+    #Утренняя пробежка
+#x = int(input())
+#d = 0
+#while x !=0:
+ # x = int(input())
+  #d += 1
+#print(d)
+
+    #Число Фиббиначи
+#n = int(input())
+#f0 = 0
+#f1 = 1
+#for i in range(n-1):
+ #   f2 = f0 + f1
+  #  f1, f0 = f2, f1
+   # print(f2)
+
+
